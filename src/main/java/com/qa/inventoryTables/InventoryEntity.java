55b8d1a96e21c01b62e9d8ea9531c29a64d6f35a
juamal <juamal27@gmail.com>
2020-02-12 13:29:01 +0000

    more java docs

--- src/main/java/com/qa/inventoryTables/InventoryEntity.java
+++ src/main/java/com/qa/inventoryTables/InventoryEntity.java
@@ -1,21 +1,41 @@
 package com.qa.inventoryTables;
 
 
-
+/**
+ * Parent of all inventory table classes
+ * @author JuamalBlackman
+ *
+ */
 public abstract class InventoryEntity {
 	private int id;
 	
+	/**
+	 * super constructor all children must have a valid id 
+	 * use -1 for invalid id
+	 * @param id
+	 */
 	public InventoryEntity(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * id getter
+	 * @return 
+	 */
 	public int getId() {
 		return id;
 	}
+	/**
+	 * id setter
+	 * @param id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * abstract method used to set all unique attributes  
+	 */
 	public abstract void userValues();
 	
 }
