e4f6433928376bfe9507238b46eb7ea98f187236
juamal <juamal27@gmail.com>
2020-02-13 19:56:00 +0000

    tested customerdao and itemsdao

--- src/test/java/databasestest/MysqlCustomerDaoTest.java
+++ src/test/java/databasestest/MysqlCustomerDaoTest.java
@@ -27,27 +27,61 @@ public class MysqlCustomerDaoTest {
 
 
 	MysqlCustomerDao customerDao = new MysqlCustomerDao();
-	Customer customer = new Customer("awating delete");
-	
+	Customer customerName = new Customer("john");
+	Customer customer = new Customer(1);
+	Customer customerNull = new Customer();
 	@Before
 	@Test
-	public void initConnectionTest() {
+	public void initCustomersConnectionTest() {
 		this.database.init("jdbc:mysql://35.187.121.181:3306/inventory_test", "root_tester", "testy");
 		
 	}
-	
 	@Test
 	public void createTest() {
-		Customer customer = new Customer("john");
-		customerDao.create(customer, database);
-		Mockito.verify(database, Mockito.times(1)).query("INSERT INTO customers(name) VALUES('" + customer.getName() + "')");
+		customerDao.create(customerName, database);
+		Mockito.verify(database, Mockito.times(1)).query("INSERT INTO customers(name) VALUES('" + customerName.getName() + "')");
+	}
+	@Test
+	public void deleteTest() {
+		customerDao.delete(customer, database);
+		Mockito.verify(database, Mockito.times(1)).query("DELETE FROM customers where id='" + customer.getId() + "'");
+	}
+	@Test
+	public void deleteFailTest() {
+		customerDao.delete(customerNull, database);
+		Mockito.verify(database, Mockito.times(0)).query("DELETE FROM customers where id='" + customerNull.getId() + "'");
+	}
+	
+	
+	@Test
+	public void updateTest() {
+		customer.setName("name");
+		customerDao.update(customer, database);
+		Mockito.verify(database, Mockito.times(1)).query("Update customers set name='" + customer.getName() + "' where id='" + customer.getId() + "'");
 	}
 	
 	@Test
-	public void deleteTest() {
-		Customer customer = new Customer(1);
-		customerDao.create(customer, database);
-		Mockito.verify(database, Mockito.times(1)).query("INSERT INTO customers(name) VALUES('" + customer.getName() + "')");
+	public void updateFailTest() {
+		customerDao.update(customerNull, database);
+		Mockito.verify(database, Mockito.times(0)).query("Update customers set name='" + customerNull.getName() + "' where id='" + customerNull.getId() + "'");
+	}
+	
+	@Test
+	public void readAllTest(){
+		customerDao.read(database);
+		Mockito.verify(database, Mockito.times(1)).selectQuery("SELECT * FROM customers");
+	}
+	
+	@Test
+	public void readIdTest() {
+		customerDao.read(database, customer.getId());
+		Mockito.verify(database, Mockito.times(1)).selectQuery("SELECT * FROM customers WHERE id = '" + customer.getId() + "'");
+	}
+	
+	@Test
+	public void readIdFailTest() {
+		customerDao.read(database, customerNull.getId());
+		Mockito.verify(database, Mockito.times(0)).selectQuery("SELECT * FROM customers WHERE id = '" + customerNull.getId() + "'");
 	}
 	
 	
